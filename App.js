4992c1eeddc2648adee10071a7cf57f0663db828
abhishek <abhishekkumar307@gmail.com>
2023-01-05 15:13:34 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,24 @@
+const heading = React.createElement(
+    'h1',{
+        id:"title"
+    },'Heading 1'
+);
+
+console.log(heading);
+
+const heading2 = React.createElement(
+    'h2',{
+        id: 'title1'
+    } , 'Heading 2'
+);
+
+const container = React.createElement(
+    'div',{
+        id: 'container'
+    },
+    [heading,heading2]
+
+);
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(container);
